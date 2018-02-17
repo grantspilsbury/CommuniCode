0be7b01bd3cd89f4b4606d7ddbd3cf3ff7be53c5
Grant Spilsbury <grantspilsbury@gmail.com>
2018-02-17 16:06:31 +1100

    Added socket and webrtc connections

--- client/src/components/Classroom.jsx
+++ client/src/components/Classroom.jsx
@@ -8,26 +8,27 @@ class Classroom extends React.Component {
   constructor(props) {
     super(props);
     this.socket = io.connect('localhost:3000');
+    this.startVideo = this.startVideo.bind(this);
     this.webrtc = new SimpleWebRTC({
-      localVideoEL: 'localVideo',
-      remoteVideosEL: 'remoteVideos',
-      autoRequestMedia: true
+      localVideoEl: 'localVideo',
+      remoteVideosEl: 'remoteVideos',
+      autoRequestMedia: true,
     });
+    this.state = {
+      channelId: '12345'
+    }
   }
 
   componentDidMount() {
-    const channelId = '12345';
     const context = this;
-
     this.socket.on('connect', () => {
       console.log('Connecting');
-      context.socket.emit('client:joinChannel', channelId, (err) => {
+      context.socket.emit('client:joinChannel', context.state.channelId, (err) => {
+        console.log('Trying to join channel');
         if (!err) {
           console.log('Joined channel');
-          context.webrtc.on('readyToCall', () => {
-            context.webrtc.joinRoom(channelId);
-          });
         } else {
+          console.log('Error in trying to join a channel', err);
           context.socket.disconnect();
         }
       });
@@ -38,12 +39,20 @@ class Classroom extends React.Component {
     });
   }
 
+  startVideo() {
+    var context = this;
+    this.webrtc.on('readyToCall', function () {
+      context.webrtc.joinRoom(context.state.channelId);
+      console.log('Started video');
+    });
+  }
+
   render() {
+    this.startVideo();
     return (
       <Grid celled padded style={{height: '100vh'}}>
         <Grid.Column width={8}>
           <Grid.Row style={{height: '33%'}}>
-            <p>Video</p>
             <div>
               <video id='localVideo'></video>
             </div>
--- server/index.js
+++ server/index.js
@@ -1,10 +1,38 @@
 const http = require('http');
+const path = require('path');
 const socket = require('socket.io');
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 const server = http.Server(app);
 const io = socket(server);
+
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root: path.join(__dirname, '../client/dist') });
+});
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('client:joinChannel', (channelId, cb) => {
+    console.log('Trying to join channel');
+    socket.join(channelId, (err) => {
+      if (err) {
+        console.log(err);
+        cb(err);
+      } else {
+        console.log('Joined channel');
+        cb(null);
+      }
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Disconnecting');
+  });
+});
+
