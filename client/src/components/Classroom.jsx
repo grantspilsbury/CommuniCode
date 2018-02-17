3437b38eab1d677e8310c7412abc81e51d37ca67
Grant Spilsbury <grantspilsbury@gmail.com>
2018-02-17 15:29:34 +1100

    Added webrtc

--- client/src/components/Classroom.jsx
+++ client/src/components/Classroom.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import { Link, Switch, Route } from 'react-router-dom';
 import { Grid, Button, Container, Input } from 'semantic-ui-react';
+import io from 'socket.io-client';
+import SimpleWebRTC from 'simplewebrtc';
 
 class Classroom extends React.Component {
   constructor(props) {
     super(props);
+    this.socket = io.connect('localhost:3000');
+    this.webrtc = new SimpleWebRTC({
+      localVideoEL: 'localVideo',
+      remoteVideosEL: 'remoteVideos',
+      autoRequestMedia: true
+    });
+  }
+
+  componentDidMount() {
+    const channelId = '12345';
+    const context = this;
+
+    this.socket.on('connect', () => {
+      console.log('Connecting');
+      context.socket.emit('client:joinChannel', channelId, (err) => {
+        if (!err) {
+          console.log('Joined channel');
+          context.webrtc.on('readyToCall', () => {
+            context.webrtc.joinRoom(channelId);
+          });
+        } else {
+          context.socket.disconnect();
+        }
+      });
+    });
+
+    this.socket.on('disconnect', () => {
+      console.log('Disconnecting');
+    });
   }
 
   render() {
@@ -13,6 +44,10 @@ class Classroom extends React.Component {
         <Grid.Column width={8}>
           <Grid.Row style={{height: '33%'}}>
             <p>Video</p>
+            <div>
+              <video id='localVideo'></video>
+            </div>
+            <div id='remoteVideos'></div>
           </Grid.Row>
           <Grid.Row style={{height: '33%'}}>
             <p>Transcription</p>
