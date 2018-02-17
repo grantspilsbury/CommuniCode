731000debb2c48fbfb9be1d0077e4303bf613b0d
Grant Spilsbury <grantspilsbury@gmail.com>
2018-02-17 13:57:02 +1100

    Added frontend

---
+++ client/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link, Switch, Route } from 'react-router-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>CommCode</h1>
+    );
+  }
+}
+
+export default App;
